fef3bd7e957f061470dbf8968d0dcfc693a1d55d
slyao <slyao@tencent.com>
2020-03-04 21:00:01 +0800

    yi dui la ji

--- k8s.sh
+++ k8s.sh
@@ -5,46 +5,51 @@
 brew cask install switchhosts
 
 ## helm
-# https://helm.sh/docs/using_helm/#installing-helm
-brew install kubernetes-helm
-helm init
+# https://helm.sh/docs/intro/install/
+brew install helm
+
+## initialize a helm chart repository
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
 
 ## ingress
 # https://github.com/helm/charts/tree/master/stable/nginx-ingress
-helm install stable/nginx-ingress \
+helm install --generate-name stable/nginx-ingress \
     --set controller.metrics.enabled=true
 
+
 ## cert-manager
-# https://docs.cert-manager.io/en/latest/getting-started/install/kubernetes.html
-kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.10/deploy/manifests/00-crds.yaml
-kubectl label namespace default certmanager.k8s.io/disable-validation=true
+# https://cert-manager.io/docs/installation/kubernetes/
+kubectl apply --validate=false -f https://raw.githubusercontent.com/jetstack/cert-manager/v0.13.1/deploy/manifests/00-crds.yaml
+kubectl create namespace cert-manager
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
-helm install jetstack/cert-manager
+helm install --generate-name jetstack/cert-manager \
+    --namespace cert-manager
 
 ## kubeapps
-# https://github.com/kubeapps/kubeapps/tree/master/chart/kubeapps
+# https://github.com/kubeapps/kubeapps/blob/master/docs/user/getting-started.md
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
-helm install bitnami/kubeapps \
+helm install --generate-name bitnami/kubeapps \
     --set ingress.enabled=true \
-    --set ingress.certManager=true
+    --set ingress.certManager=true \
+    --set useHelm3=true 
 
 kubectl create serviceaccount kubeapps-operator
 kubectl create clusterrolebinding kubeapps-operator --clusterrole=cluster-admin --serviceaccount=default:kubeapps-operator
-kubectl get secret $(kubectl get serviceaccount kubeapps-operator -o jsonpath='{.secrets[].name}') -o jsonpath='{.data.token}' | base64 --decode && echo
+kubectl get secret $(kubectl get serviceaccount kubeapps-operator -o jsonpath='{range .secrets[*]}{.name}{"\n"}{end}' | grep kubeapps-operator-token) -o jsonpath='{.data.token}' -o go-template='{{.data.token | base64decode}}' && echo
 
 ## kubernetes-dashboard
 # https://github.com/helm/charts/tree/master/stable/kubernetes-dashboard
-helm install stable/kubernetes-dashboard \
+helm install --generate-name stable/kubernetes-dashboard \
     --set enableSkipLogin=true \
     --set ingress.enabled=true \
-    --set ingress.hosts[0]=kubernetes-dashboard.local \
+    --set ingress.hosts={"kubernetes-dashboard.local"} \
     --set ingress.annotations."kubernetes\.io/ingress\.class"=nginx \
-    --set ingress.annotations."kubernetes\.io/tls-acme"="true" \
-    --set ingress.annotations."nginx\.ingress\.kubernetes\.io/backend-protocol"="HTTPS" \
-    --set ingress.tls[0].secretName=kubernetes-dashboard-tls \
-    --set ingress.tls[0].hosts[0]=kubernetes-dashboard.local \
+    --set ingress.annotations."kubernetes\.io/tls-acme"=\"true\" \
+    --set ingress.annotations."nginx\.ingress\.kubernetes\.io/backend-protocol"=HTTPS \
+    --set ingress.tls\[0\].secretName="kubernetes-dashboard-tls" \
+    --set ingress.tls\[0\].hosts={"kubernetes-dashboard.local"} \
     --set rbac.clusterAdminRole=true
 
 ## prometheus-operator
