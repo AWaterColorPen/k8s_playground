f5aa36fb138d57d711fb26fbd56aa222ec9ba8ab
slyao <slyao@tencent.com>
2020-05-12 14:39:32 +0800

    add a set

--- common-app.sh
+++ common-app.sh
@@ -54,4 +54,5 @@ helm install --generate-name stable/kubernetes-dashboard \
 ## chartmuseum
 # https://github.com/helm/charts/tree/master/stable/chartmuseum
 helm install --generate-name stable/chartmuseum \
+    --set env.open.DISABLE_API=false \
     --set persistence.enabled=true
